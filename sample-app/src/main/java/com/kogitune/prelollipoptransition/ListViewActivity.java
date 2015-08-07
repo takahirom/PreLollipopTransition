4731cbf02f4417303cd193eef21c8ea78480bfcb
takahirom <takam.dev@gmail.com>
2015-08-08 00:13:46 +0900

    Remove unnecessary method

--- sample-app/src/main/java/com/kogitune/prelollipoptransition/ListViewActivity.java
+++ sample-app/src/main/java/com/kogitune/prelollipoptransition/ListViewActivity.java
@@ -51,25 +51,4 @@ public class ListViewActivity extends AppCompatActivity {
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_list_view, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
