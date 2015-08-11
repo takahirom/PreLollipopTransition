c177b08771decb01f03cbabeca22ccc523f24607
takahirom <takam.dev@gmail.com>
2015-08-12 01:24:56 +0900

    Remove unnecessary method

--- sample-app/src/main/java/com/kogitune/prelollipoptransition/SubActivity2.java
+++ sample-app/src/main/java/com/kogitune/prelollipoptransition/SubActivity2.java
@@ -20,29 +20,6 @@ public class SubActivity2 extends ActionBarActivity {
         exitTransition = ActivityTransition.with(getIntent()).to(findViewById(R.id.sub_imageView)).start(savedInstanceState);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_sub, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     @Override
     public void onBackPressed() {
         exitTransition.exit(this);
