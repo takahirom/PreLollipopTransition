d9c2d0c75a21e94b4eb4ff35f55fb390f4a2a51a
takahirom <takam.dev@gmail.com>
2015-10-08 00:38:08 +0900

    Use AppCompatActivity

--- sample-app/src/main/java/com/kogitune/prelollipoptransition/SubActivity.java
+++ sample-app/src/main/java/com/kogitune/prelollipoptransition/SubActivity.java
@@ -1,15 +1,13 @@
 package com.kogitune.prelollipoptransition;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
 
 import com.kogitune.activity_transition.ActivityTransition;
 import com.kogitune.activity_transition.ExitActivityTransition;
 
 
-public class SubActivity extends ActionBarActivity {
+public class SubActivity extends AppCompatActivity {
 
     private ExitActivityTransition exitTransition;
 
@@ -18,29 +16,6 @@ public class SubActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub);
         exitTransition = ActivityTransition.with(getIntent()).to(findViewById(R.id.sub_imageView)).start(savedInstanceState);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_sub, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
