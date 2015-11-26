e4b5bf77fc519ae1b3c7db4178b2ba49d3f0b3f9
takahirom <takam.dev@gmail.com>
2015-11-27 02:14:40 +0900

    Add LICENSE Text to source code.
    Add LICENSE Text to source code.

--- pre-lollipop-activity-transition/src/main/java/com/kogitune/activity_transition/fragment/FragmentTransition.java
+++ pre-lollipop-activity-transition/src/main/java/com/kogitune/activity_transition/fragment/FragmentTransition.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2015 takahirom
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package com.kogitune.activity_transition.fragment;
 
 import android.animation.TimeInterpolator;
@@ -10,9 +27,6 @@ import android.view.animation.DecelerateInterpolator;
 import com.kogitune.activity_transition.core.MoveData;
 import com.kogitune.activity_transition.core.TransitionAnimation;
 
-/**
- * Created by takam on 2015/03/26.
- */
 public class FragmentTransition {
     private static TimeInterpolator interpolator;
     private int duration = 1000;
--- pre-lollipop-activity-transition/src/main/java/com/kogitune/activity_transition/fragment/FragmentTransitionLauncher.java
+++ pre-lollipop-activity-transition/src/main/java/com/kogitune/activity_transition/fragment/FragmentTransitionLauncher.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2015 takahirom
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package com.kogitune.activity_transition.fragment;
 
 import android.app.Fragment;
@@ -8,9 +25,6 @@ import android.view.View;
 
 import com.kogitune.activity_transition.core.TransitionBundleFactory;
 
-/**
- * Created by takam on 2015/03/26.
- */
 public class FragmentTransitionLauncher {
     private static final String TAG = "TransitionLauncher";
 
