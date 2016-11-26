2ed335a74e269371948829fc863a8ca1c44612ab
Joe <lovejjfg@163.com>
2016-11-26 21:35:33 +0800

    add license in ListViewFragment.java

--- sample-app/src/main/java/com/kogitune/prelollipoptransition/fragment/ListViewFragment.java
+++ sample-app/src/main/java/com/kogitune/prelollipoptransition/fragment/ListViewFragment.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2015 takahirom
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 package com.kogitune.prelollipoptransition.fragment;
 
 import android.content.Intent;
@@ -15,11 +31,6 @@ import com.kogitune.activity_transition.ActivityTransitionLauncher;
 import com.kogitune.prelollipoptransition.ListViewActivity;
 import com.kogitune.prelollipoptransition.R;
 import com.kogitune.prelollipoptransition.SubActivity;
-
-/**
- * Created by Joe on 2016/11/24.
- * Email lovejjfg@gmail.com
- */
 
 public class ListViewFragment extends Fragment {
     @Nullable
